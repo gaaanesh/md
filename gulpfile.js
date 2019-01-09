a6c3f0e1d445520c2224012b55f449aecae5fae5
gaaanesh <ganesh.badhe5@gmail.com>
2019-01-09 08:35:53 +0530

    rediret to 'in' home page

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'in/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("in/css"))
         .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /src/js folder
+// Move the javascript files into our /in/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("in/js"))
         .pipe(browserSync.stream());
 });
 
@@ -21,11 +21,11 @@ gulp.task('js', function() {
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./src"  
+        server: "./"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'in/scss/*.scss'], ['sass']);
+    gulp.watch("in/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['js','serve']);
