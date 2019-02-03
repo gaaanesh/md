bed1621d4d698467ea232179e8a0ee8d3864748f
gaaanesh <ganesh.badhe5@gmail.com>
2019-02-03 20:03:22 +0530

    in folder changed to docs for github

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,30 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+// var rigger      = require('gulp-rigger');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'in/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("in/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /in/js folder
+// Move the javascript files into our /docs/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest("in/js"))
+        .pipe(gulp.dest("docs/js"))
         .pipe(browserSync.stream());
 });
+
+// gulp.task('rigger', function () {
+//    var file_src = '*.html';
+//     gulp.src(file_src)
+//         .pipe(rigger())
+//         .pipe(gulp.dest("*"))
+//         .pipe(browserSync.stream());
+// });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -24,7 +33,7 @@ gulp.task('serve', ['sass'], function() {
         server: "./"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'in/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'], ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
